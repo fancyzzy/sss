bff77c804784519f05ab639f0ab7f128b8ad78b0
Felix Zhang <felix.zhang@nokia-sbell.com>
2017-12-04 17:26:24 +0800

    Add color to Issue Category
    Add CRs column too in the show_result

--- my_resources.py
+++ my_resources.py
@@ -69,6 +69,7 @@ This is all the resources location
 my_color_blue = '#%02x%02x%02x' % (51,153,255)
 my_color_blue_office ='#%02x%02x%02x' % (43,87,154) 
 my_color_green = '#%02x%02x%02x' % (192,233,17)
+my_color_orange = '#%02x%02x%02x' % (210,71,38)
 #for terminate searching
 l_threads = []
 PREDIFINED_KEYWORD = 'keywords.csv'
